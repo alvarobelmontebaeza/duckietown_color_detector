3bc0168477db45773dad9307d43bfd0a342ca0a8
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-10-18 13:29:59 +0200

    Changed color detection method from color histogram to color thresholds

--- color_detector.py
+++ color_detector.py
@@ -24,18 +24,80 @@ while(True):
         last_row = int((i+1) * split_size)
         colorHist = []
         
-        segment = hsv_image[first_row:last_row, :]
+        segment = hsv_image[first_row:last_row, :,:]
 
-        # Obtain color histogram. 
-        # params: (image,channels,...,bins,range)
-        colorHist = cv2.calcHist([hsv_image],[0,1],None,[360,256],[0,360,0,256])
+        # ----------- APPLY COLOR FILTERING ----------
+        # Check for the current image segment what is 
+        # the most present color among the most common
+        # colors in duckietown: Black (road), Yellow, 
+        # White (lines/leds), Red (duckiebots), Blue
 
-        # obtain current most detected color
-        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(colorHist)
+        ####### Apply Black filtering ########
 
-        print("Image segment " + str(i) +":")
-        print("Most common color: " + str(maxLoc))
-        print("----------------------------------")
+        # define range of black color in HSV
+        lower_black = np.array([0,0,0])
+        upper_black = np.array([0,0,75])
+
+        # Threshold the HSV image to get only black colors
+        mask_black = cv2.inRange(hsv_image, lower_black, upper_black)
+
+        # Bitwise-AND mask and original image
+        black = cv2.bitwise_and(hsv_image,hsv_image, mask= mask_black)
+
+        ####### Apply Blue filtering #########
+
+        # define range of blue color in HSV
+        lower_blue = np.array([110,50,50])
+        upper_blue = np.array([130,255,255])
+
+        # Threshold the HSV image to get only blue colors
+        mask_blue = cv2.inRange(hsv_image, lower_blue, upper_blue)
+
+        # Bitwise-AND mask and original image
+        blue = cv2.bitwise_and(hsv_image,hsv_image, mask= mask_blue)
+        
+        ########## Apply Yellow filtering ##########
+
+        # define range of yellow color in HSV
+        lower_yellow = np.array([20,50,50])
+        upper_yellow = np.array([40,255,255])
+
+        # Threshold the HSV image to get only yellow colors
+        mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+        # Bitwise-AND mask and original image
+        yellow = cv2.bitwise_and(hsv_image,hsv_image, mask= mask_yellow)
+
+        # Apply White filtering
+
+        # define range of white color in HSV
+        lower_white = np.array([0,0,125])
+        upper_white = np.array([0,0,255])
+
+        # Threshold the HSV image to get only white colors
+        mask_white = cv2.inRange(hsv_image, lower_white, upper_white)
+
+        # Bitwise-AND mask and original image
+        white = cv2.bitwise_and(hsv_image,hsv_image, mask= mask_white)
+
+        # Apply Red filtering.
+
+        # define range of red color in HSV
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([10,255,255])
+
+        mask1 = cv2.inRange(hsv_image, lower_red, upper_red)
+
+        lower_red = np.array([170,50,50])
+        upper_red = np.array([179,255,255])
+
+        mask2 = cv2.inRange(hsv_image, lower_red, upper_red)
+
+        # mix both masks to obtain full red range mask
+        mask_red = mask1 or mask2
+
+        # Bitwise-AND mask and original image
+        red = cv2.bitwise_and(hsv_image,hsv_image, mask= mask_red)
 
     # Set 1 Hz frequency
     sleep(1)
