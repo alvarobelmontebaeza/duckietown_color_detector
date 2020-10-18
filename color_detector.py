75a93dfb4339b646751ab10c5c9a7d19a24ee983
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-10-18 11:08:48 +0200

    Removed visual display as DB does not have one

--- color_detector.py
+++ color_detector.py
@@ -17,22 +17,23 @@ while(True):
     hsv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     # h, s, v = cv2.split(hsv_image)
     split_size = len(hsv_image)/n_splits
-    colorHist = np.array([[],[],[]])
 
     # Obtain color distribution in each horizontal split.
     for i in range(n_splits):
         first_row = int(i*split_size)
-        last_row = int((i+1) * split_size))
+        last_row = int((i+1) * split_size)
         
         segment = hsv_image[first_row:last_row, :]
 
         # Obtain color histogram. 
         # params: (image,channels,...,bins,range)
-        colorHist[i,:,:] = cv2.calcHist([hsv_image],[0,1],None,[180,256],[0,180,0,256])
+        colorHist = cv2.calcHist([hsv_image],[0,1],None,[180,256],[0,180,0,256])
     
-    cv2.imshow(frame)
-    plt.imshow(colorHist,interpolation = 'nearest')
-    plt.show()
+    # cv2.imshow('duckieVision',frame)
+    # plt.imshow(colorHist,interpolation = 'nearest')
+    # plt.show()
+
+    print(colorHist)
 
     # Set 1 Hz frequency
     sleep(1)
